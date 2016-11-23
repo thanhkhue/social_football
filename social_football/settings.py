0f0933ec4db491e6101be60097e6e94821c1a464
khuebui <thanhkhuebkdn2012@gmail.com>
2016-11-23 14:16:48 +0700

    [FIX] Trying to fix setup Gunicorn on server Heroku

--- social_football/settings.py
+++ social_football/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=0ox@)u-rh4py+%92+u+(-p6-9lr(942fes2q&7x)i#edo!fz$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
